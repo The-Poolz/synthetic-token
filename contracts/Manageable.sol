355b388e7a2798b7f271bbf6372c233989a073d8
Ashwin Arora <ashwinarora48@gmail.com>
2021-11-24 18:44:32 +0530

    added locking functionality

---
+++ contracts/Manageable.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+import "poolz-helper/contracts/ERC20Helper.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "poolz-helper/contracts/GovManager.sol";
+
+contract Manageable is ERC20Helper, GovManager{
+
+    event LockingDetails(address TokenAddress, uint256 Amount, uint8 TotalUnlocks);
+
+    address public OriginalTokenAddress;
+    address public LockedDealAddress;
+
+    struct lockDetails {
+        uint64 unlockTime;
+        uint ratio;
+    }
+
+    mapping(uint8 => lockDetails) public LockDetails;
+
+    uint8 public totalUnlocks;
+    uint public totalOfRatios;
+
+    function SetLockingDetails(
+        address _tokenAddress,
+        uint256 _amount,
+        uint64[] calldata _unlockTimes,
+        uint8[] calldata _ratios
+    ) external onlyOwnerOrGov {
+        require(_unlockTimes.length == _ratios.length, "Both arrays should have same length.");
+        require(_unlockTimes.length > 0, "Array length should be greater than 0");
+        require(totalUnlocks == 0, "Unlock Data Already Present");
+        OriginalTokenAddress = _tokenAddress;
+        TransferInToken(_tokenAddress, msg.sender, _amount);
+        for(uint8 i=0; i<_unlockTimes.length ; i++){
+            LockDetails[i] = lockDetails(_unlockTimes[i], _ratios[i]);
+            totalOfRatios = SafeMath.add(totalOfRatios, _ratios[i]);
+        }
+        totalUnlocks = uint8(_unlockTimes.length);
+        emit LockingDetails(_tokenAddress, _amount, totalUnlocks);
+    }
+
+    function SetLockedDealAddress(address lockedDeal) external onlyOwnerOrGov {
+        LockedDealAddress = lockedDeal;
+    }
+
+}
--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,19 +5,24 @@ pragma solidity ^0.6.0;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Capped.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "./Manageable.sol";
+import "poolz-helper/contracts/ERC20Helper.sol";
+import "poolz-helper/contracts/ILockedDeal.sol";
 
-contract POOLZSYNT is ERC20, ERC20Capped, ERC20Burnable {
+contract POOLZSYNT is ERC20, ERC20Capped, ERC20Burnable, Manageable {
     // uint256 _cap = 2000000 * 10**18;\
 
     event Becon(bytes Address, uint256 Amount);
 
-    constructor(string memory _name, string memory _symbol, uint _cap, address _owner)
+    constructor(string memory _name, string memory _symbol, uint _cap, uint8 _decimals, address _owner)
         public
         ERC20(_name, _symbol)
-        ERC20Capped(_cap * 10**18)
+        ERC20Capped(_cap * 10**uint(_decimals))
     {
-        _setupDecimals(18);
-        _mint(_owner, _cap * 10**18);
+        require(_decimals <= 18, "Decimal more than 18");
+        _setupDecimals(_decimals);
+        _mint(_owner, _cap * 10**uint(_decimals));
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 amount)
@@ -33,5 +38,21 @@ contract POOLZSYNT is ERC20, ERC20Capped, ERC20Burnable {
     function ActivateBeacon(bytes memory _Address, uint256 _Amount) public {
         burn(_Amount);
         emit Becon(_Address, _Amount);
+    }
+
+    function ActivateSynthetic() external {
+        require(totalUnlocks != 0, "Original Token not Ready");
+        for(uint8 i=0 ; i<totalUnlocks ; i++){
+            uint amount = SafeMath.div(
+                SafeMath.mul( balanceOf(_msgSender()), LockDetails[i].ratio ),
+                totalOfRatios
+            );
+            burn(amount);
+            if(LockDetails[i].unlockTime <= now){
+                TransferToken(OriginalTokenAddress, _msgSender(), amount );
+            } else {
+                ILockedDeal(LockedDealAddress).CreateNewPool(OriginalTokenAddress, LockDetails[i].unlockTime, amount, _msgSender());
+            }
+        }
     }
 }
