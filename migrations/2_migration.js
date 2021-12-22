bf299ec10217b3861b3549953f401aef1de1c839
Ashwin Arora <ashwinarora48@gmail.com>
2021-12-22 15:16:23 +0530

    commenting out migrations

--- migrations/2_migration.js
+++ migrations/2_migration.js
@@ -2,6 +2,6 @@ const Token = artifacts.require("POOLZSYNT")
 const TestToken = artifacts.require("OriginalToken");
 
 module.exports = function(deployer) {
-    deployer.deploy(Token, "TestEnvelop", "TENV", "10000", '18', "0x5f14560149dA11b499C1569f03a1d6C34a0a6c3C")
-    deployer.deploy(TestToken, 'OrgToken', 'ORGT')
+    // deployer.deploy(Token, "TestEnvelop", "TENV", "10000", '18', "0x5f14560149dA11b499C1569f03a1d6C34a0a6c3C")
+    // deployer.deploy(TestToken, 'OrgToken', 'ORGT')
 }
